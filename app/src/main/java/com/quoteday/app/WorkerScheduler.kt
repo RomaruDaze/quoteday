4985150c645fe14654841d14c7aaf0127b82dd7c
RomaruDaze <20122077@kaishi-pu.ac.jp>
2026-05-08 06:52:25 +0900

    Auto-save: 2026-05-08 06:52

--- app/src/main/java/com/quoteday/app/WorkerScheduler.kt
+++ app/src/main/java/com/quoteday/app/WorkerScheduler.kt
@@ -13,16 +13,14 @@ import java.util.concurrent.TimeUnit
 private const val WORK_NAME = "daily_quote_work"
 
 fun Context.scheduleDailyQuote() {
+    val hour = SettingsPrefs.getNotificationHour(this)
+    val minute = SettingsPrefs.getNotificationMinute(this)
+
     val now = LocalDateTime.now()
-    val target = now.toLocalDate().atTime(LocalTime.of(8, 0))
+    val targetTime = LocalTime.of(hour, minute)
+    val target = now.toLocalDate().atTime(targetTime)
 
-    // If 8 AM already passed today, schedule for tomorrow
-    val next = if (now.toLocalTime().isAfter(LocalTime.of(8, 0))) {
-        target.plusDays(1)
-    } else {
-        target
-    }
-
+    val next = if (now.toLocalTime().isAfter(targetTime)) target.plusDays(1) else target
     val initialDelay = Duration.between(now, next).toMinutes()
 
     val request = PeriodicWorkRequestBuilder<QuoteDailyWorker>(1, TimeUnit.DAYS)
@@ -34,4 +32,8 @@ fun Context.scheduleDailyQuote() {
         ExistingPeriodicWorkPolicy.UPDATE,
         request
     )
+}
+
+fun Context.cancelDailyQuote() {
+    WorkManager.getInstance(this).cancelUniqueWork(WORK_NAME)
 }
