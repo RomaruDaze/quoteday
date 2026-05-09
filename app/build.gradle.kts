ad3ef77fb1c4ab253e239c3656c2a90ba4363d27
RomaruDaze <20122077@kaishi-pu.ac.jp>
2026-05-09 19:43:01 +0900

    Auto-save: 2026-05-09 19:43

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icons.extended)
-    implementation(libs.androidx.ui.text.google.fonts)
     implementation(libs.androidx.work.runtime.ktx)
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.firestore.ktx)
