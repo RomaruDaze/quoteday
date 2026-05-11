5fd77e64a28bd64acd1765eef30af1c29b23fe2e
RomaruDaze <20122077@kaishi-pu.ac.jp>
2026-05-11 19:52:31 +0900

    Auto-save: 2026-05-11 19:52

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.quoteday.app"
+        applicationId = "com.romarudaze.quoteday"
         minSdk = 26
         targetSdk = 35
         versionCode = 3
