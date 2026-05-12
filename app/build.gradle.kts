d524ff2b0a383c7f6f241fe98a52698408072d9e
RomaruDaze <20122077@kaishi-pu.ac.jp>
2026-05-12 10:17:12 +0900

    Auto-save: 2026-05-12 10:17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.romarudaze.quoteday"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.2"
+        versionCode = 6
+        versionName = "1.3"
     }
 
     signingConfigs {
