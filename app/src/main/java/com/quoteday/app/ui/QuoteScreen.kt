ec385d4e08f267b993078e0cbcee208a9e3f2584
RomaruDaze <20122077@kaishi-pu.ac.jp>
2026-05-12 14:53:46 +0900

    Auto-save: 2026-05-12 14:53

--- app/src/main/java/com/quoteday/app/ui/QuoteScreen.kt
+++ app/src/main/java/com/quoteday/app/ui/QuoteScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalClipboardManager
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontFamily
