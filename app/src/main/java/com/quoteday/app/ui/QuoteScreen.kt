98d977b6c6d0d9afad2dc9c852b297ba608ec863
RomaruDaze <20122077@kaishi-pu.ac.jp>
2026-05-12 13:22:09 +0900

    Auto-save: 2026-05-12 13:22

--- app/src/main/java/com/quoteday/app/ui/QuoteScreen.kt
+++ app/src/main/java/com/quoteday/app/ui/QuoteScreen.kt
@@ -53,7 +53,6 @@ fun QuoteScreen(viewModel: QuoteViewModel, onSettingsClick: () -> Unit) {
     val todayQuote by viewModel.todayQuote.collectAsState()
     val isPremium by viewModel.isPremium.collectAsState()
     val limitReached by viewModel.limitReached.collectAsState()
-    val showUpgradePrompt by viewModel.showUpgradePrompt.collectAsState()
     var selectedTab by remember { mutableStateOf(Tab.Today) }
     var showDialog by remember { mutableStateOf(false) }
     var editingQuote by remember { mutableStateOf<Quote?>(null) }
