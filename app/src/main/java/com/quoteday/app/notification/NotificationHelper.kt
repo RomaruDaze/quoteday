edec77bf69cbd8878dd3957863578620497f16ec
RomaruDaze <20122077@kaishi-pu.ac.jp>
2026-05-15 21:39:25 +0900

    Auto-save: 2026-05-15 21:39

--- app/src/main/java/com/quoteday/app/notification/NotificationHelper.kt
+++ app/src/main/java/com/quoteday/app/notification/NotificationHelper.kt
@@ -2,9 +2,12 @@ package com.quoteday.app.notification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
 import androidx.core.app.NotificationCompat
+import com.quoteday.app.MainActivity
 import com.quoteday.app.R
 
 object NotificationHelper {
