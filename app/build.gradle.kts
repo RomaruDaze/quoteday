0e26d15aa80da36ac78663006d08e1788c6eeef3
RomaruDaze <20122077@kaishi-pu.ac.jp>
2026-08-16 17:37:18 +0900

    Auto-save: 2026-08-16 17:37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.quoteday.app"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.romarudaze.quoteday"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 10
         versionName = "1.4"
     }
